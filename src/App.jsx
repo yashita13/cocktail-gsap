3ddc8d3741f922c184414f498f9e2218312a9804
Yashita Bahrani <bahraniyashita@gmail.com>
2025-06-23 17:08:53 +0530

    added animations to hero section -1

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
         <main>
             <Nav />
             <Hero />
+            <div className="h-dvh bg-black" />
         </main>
     );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,41 @@
 import {useGSAP} from "@gsap/react";
+import {gsap} from "gsap";
+import {SplitText} from "gsap/all";
 
 const Hero = () => {
-    // useGSAP()
+    useGSAP(()=>{
+        const heroSplit=new SplitText('.title', {type:'chars, words'});
+        const paraSplit=new SplitText('.subtitle', {type:'lines'});
+
+        heroSplit.chars.forEach((char)=>char.classList.add('text-gradient'));
+
+        gsap.from(heroSplit.chars,{
+            yPercent:100,
+            duration:1.8,
+            ease:'expo.out',
+            stagger:0.06
+        })
+
+        gsap.from(paraSplit.lines,{
+            opacity:0,
+            yPercent:100,
+            duration:1.8,
+            ease:'expo.out',
+            stagger:0.06,
+            delay:1
+        })
+
+        gsap.timeline({
+            scrollTrigger:{
+                trigger:'#hero',
+                start:'top top',
+                end:'bottom top',
+                scrub:true,
+            }
+        })
+            .to('.right-leaf', {y:200},0)
+            .to('.left-leaf', {y:-200},0)
+    })
 
     return (
         <>
