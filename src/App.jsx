292469ce636be812c070e942affb59ff9a4d4300
Yashita Bahrani <bahraniyashita@gmail.com>
2025-06-24 02:40:03 +0530

    implemented cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {gsap} from "gsap";
 import {ScrollTrigger, SplitText} from "gsap/all";
 import Nav from "./components/Nav.jsx";
 import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +13,7 @@ const App = () => {
             <Nav />
             <Hero />
             {/*<div className="h-dvh bg-black" />*/}
+            <Cocktails />
         </main>
     );
 };
