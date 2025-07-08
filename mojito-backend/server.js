2a2a69ffb1daec231064ef7892f7fae17e3a0859
Yashita Bahrani <bahraniyashita@gmail.com>
2025-07-09 02:14:45 +0530

    updates for app.use-cors

--- mojito-backend/server.js
+++ mojito-backend/server.js
@@ -10,7 +10,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: [
+        'https://cocktail-gsap-ten.vercel.app',
+        'http://localhost:5174' // your React dev server
+    ],
+    methods: ['GET', 'POST'],
+    credentials: false
+}));
+
+
 app.use(express.json());
 
 // Routes
